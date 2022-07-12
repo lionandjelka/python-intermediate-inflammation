b95d7e366fe5d5f2ed34b155d33766576dd8b7f5
lionandjelka <angelkovacevic@gmail.com>
2022-07-12 15:12:42 +0200

    Docstring improvements.

--- inflammation/models.py
+++ inflammation/models.py
@@ -17,14 +17,14 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-   """Calculate the daily mean of a 2D inflammation data array for each day. :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    """Calculate the daily mean of a 2D inflammation data array for each day. :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
    :returns: An array of mean values of measurements for each day.
    """
-   return np.mean(data, axis=0)
+    return np.mean(data, axis=0)
 
 
 def daily_max(data):
-  """Calculate the daily maximum of a 2D inflammation data array for each day.
+    """Calculate the daily maximum of a 2D inflammation data array for each day.
     param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
    :returns: An array of max values of measurements for each day.
    """
@@ -32,9 +32,8 @@ def daily_max(data):
 
 
 def daily_min(data):
-   #Calculate the daily minimum of a 2D inflammation data array for each day.
-   #:param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
-   #:returns: An array of minimum values of measurements for each day.
-   #"""
-   return np.min(data, axis=0)
-
+    # Calculate the daily minimum of a 2D inflammation data array for each day.
+    #:param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    #:returns: An array of minimum values of measurements for each day.
+    # """
+    return np.min(data, axis=0)
